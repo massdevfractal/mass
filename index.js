624adf22b42c6e1e9dcbb3f8ca986deedf93fa0d
massdevfractal <massdevfractal@gmail.com>
2021-11-18 14:25:06 +0330

    jj

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
 const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json');
 
 // clears the console
 clearConsole();
+
+console.log(`
+NAME: ${pkgJSON.name}
+`);
 
 console.log("Mass Dev + Hi! I am new in node.js!!");
 
